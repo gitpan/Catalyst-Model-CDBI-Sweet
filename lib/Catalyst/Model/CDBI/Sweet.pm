8dd219e456bb2cb60b849737771e3b99dbc4b760
Marcus Ramberg <mramberg@cpan.org>
2005-10-27 15:26:59 +0000

    Import of MRAMBERG/Catalyst-Model-CDBI-Sweet-0.05 from CPAN.
    gitpan-cpan-distribution: Catalyst-Model-CDBI-Sweet
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MRAMBERG/Catalyst-Model-CDBI-Sweet-0.05.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Model/CDBI/Sweet.pm
+++ lib/Catalyst/Model/CDBI/Sweet.pm
@@ -3,9 +3,19 @@ package Catalyst::Model::CDBI::Sweet;
 use strict;
 use base qw[Class::DBI::Sweet Catalyst::Base];
 
-our $VERSION = '0.04';
+use Catalyst::Exception;
+
+our $VERSION = '0.05';
 
 *new = \&Catalyst::Base::new;
+
+sub _croak {
+    my ( $self, $message ) = @_;
+    
+    local $Carp::CarpLevel = 2;
+    
+    Catalyst::Exception->throw( message => $message );
+}
 
 1;
 
@@ -24,83 +34,24 @@ __END__
     package MyApp::Model::Article;
     use base 'MyApp::Model::CDBI';
     
-    use DateTime;
-    
-    __PACKAGE__->table('article');
-    __PACKAGE__->columns( Primary   => qw[ id ] );
-    __PACKAGE__->columns( Essential => qw[ title created_on created_by ] );
-    
-    __PACKAGE__->has_a(
-        created_on => 'DateTime',
-        inflate    => sub { DateTime->from_epoch( epoch => shift ) },
-        deflate    => sub { shift->epoch }
-    );
-    
-    # Simple search
-    
-    MyApp::Model::Article->search( created_by => 'sri', { order_by => 'title' } );
-    
-    MyApp::Model::Article->count( created_by => 'sri' );
-    
-    MyApp::Model::Article->page( created_by => 'sri', { page => 5 } );
-    
-    MyApp::Model::Article->retrieve_all( order_by => 'created_on' );
-    
-    
-    # More powerful search with deflating
-    
-    $criteria = {
-        created_on => {
-            -between => [
-                DateTime->new( year => 2004 ),
-                DateTime->new( year => 2005 ),
-            ]
-        },
-        created_by => [ qw(chansen draven gabb jester sri) ],
-        title      => {
-            -like  => [ qw( perl% catalyst% ) ]
-        }
-    };
-    
-    MyApp::Model::Article->search( $criteria, { rows => 30 } );
-    
-    MyApp::Model::Article->count($criteria);
-    
-    MyApp::Model::Article->page( $criteria, { rows => 10, page => 2 } );
-    
-    
-    # Using Catalyst::Model::CDBI::Sweet with Catalyst::Model::CDBI
-    
-    package MyApp::Model::CDBI;
-    use base 'Catalyst::Model::CDBI';
-    
-    use Catalyst::Model::CDBI::Sweet;
-    
-    __PACKAGE__->config(
-        dsn           => 'dbi:driver:dbname',
-        password      => 'username',
-        user          => 'password',
-        relationships => 1
-    );
-    
-    sub new {
-         my $class = shift;
-    
-         my $self = $class->NEXT::new(@_);
-    
-         foreach my $subclass ( $self->loader->classes ) {
-             no strict 'refs';
-             unshift @{ $subclass . '::ISA' }, 'Catalyst::Model::CDBI::Sweet';
-         }
-    
-         return $self;
-    }
-
+	... # table class config here
 
 =head1 DESCRIPTION
 
-This is the Catalyst::Model::CDBI::Sweet model class. It's built on top of 
-Class::DBI::Sweet.
+This model is a thin wrapper around L<Class::DBI::Sweet> to let you use it 
+as a Catalyst Model easily. It's similar to L<Catalyst::Model::CDBI::Plain>.
+
+If you want to use loader, you will probably want to add something like this
+to your CDBI model config section instead:
+
+           left_base_classes       => qw/Class::DBI::Sweet/,
+
+To see how you can take advantage of this module, please check out the
+L<Class::DBI::Sweet> documentation.
+
+=head1 SEE ALSO
+
+L<Class::DBI::Sweet>, L<Catalyst::Model::CDBI>, L<Catalyst>.
 
 =head1 AUTHOR
 
